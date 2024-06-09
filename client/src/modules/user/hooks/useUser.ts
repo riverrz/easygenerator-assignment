cc1b3c090daf629096f9ce698089a377d814a943
riverrz <shivam123425@gmail.com>
2024-06-09 20:00:19 +0400

    Remove stale time

--- client/src/modules/user/hooks/useUser.ts
+++ client/src/modules/user/hooks/useUser.ts
@@ -4,7 +4,6 @@ import { getUserProfile } from "../services/user.service";
 export const useUser = () => {
   const { data, isFetching } = useQuery({
     queryKey: ["user-profile"],
-    staleTime: 0,
     queryFn: () => getUserProfile(),
     refetchOnWindowFocus: false,
     throwOnError: true,
