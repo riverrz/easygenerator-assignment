c2ad4076fc0086c2aae9f9709ecb57151009875d
riverrz <shivam123425@gmail.com>
2024-06-09 13:39:17 +0400

    Add check for email already taken

--- server/src/user/services/user.service.ts
+++ server/src/user/services/user.service.ts
@@ -1,5 +1,5 @@
 import * as bcrypt from 'bcrypt';
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from '../entities/user.entity';
 import { FilterQuery, Model } from 'mongoose';
@@ -10,6 +10,15 @@ export class UserService {
 
   async create(payload: { name: string; email: string; password: string }) {
     const { name, email, password } = payload;
+
+    const isEmailTaken = await this.exists({ email });
+
+    if (isEmailTaken) {
+      throw new BadRequestException(
+        'An account with this email already exists',
+      );
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
 
     return this.userModel.create({
@@ -21,5 +30,8 @@ export class UserService {
 
   async findOne(filterExpression: FilterQuery<User>) {
     return this.userModel.findOne(filterExpression);
+  }
+  async exists(filterExpression: FilterQuery<User>) {
+    return this.userModel.exists(filterExpression);
   }
 }
