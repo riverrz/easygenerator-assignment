05b84575c6cc4a795ef69413b8f825d7da246c39
riverrz <shivam123425@gmail.com>
2024-06-09 19:35:48 +0400

    Add a logout button

--- client/src/modules/home/home.page.tsx
+++ client/src/modules/home/home.page.tsx
@@ -1,17 +1,30 @@
 import { Spinner } from "@/components/ui/spinner";
 import { useUser } from "../user/hooks/useUser";
+import { Button } from "@/components/ui/button";
+import { logout } from "@/modules/auth/services/auth.service";
 
 export const HomePage = () => {
   const { isFetching, data } = useUser();
 
+  const handleLogout = () => {
+    return logout();
+  };
+
   return (
-    <main>
+    <main className="flex justify-center p-8">
       {isFetching ? (
-        <div className="mt-8 flex justify-center">
+        <div>
           <Spinner className="h-12 w-12" />
         </div>
       ) : (
-        <h1>Welcome to the application, {data?.name}!</h1>
+        <div>
+          <h1>Welcome to the application, {data?.name}!</h1>
+          <div className="mt-4 text-center">
+            <Button variant="destructive" onClick={handleLogout}>
+              Logout
+            </Button>
+          </div>
+        </div>
       )}
     </main>
   );
