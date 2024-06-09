338cc1fb627dd26823c36b372ae6adb8c57b6955
riverrz <shivam123425@gmail.com>
2024-06-09 19:31:49 +0400

    Fix cors configuration and set cookie restrictions based on APP_ENV

--- server/src/auth/controllers/auth.controller.ts
+++ server/src/auth/controllers/auth.controller.ts
@@ -1,13 +1,16 @@
-import { Body, Controller, Post, Res, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
 import { Response } from 'express';
 import { LoginDto } from '../dtos/login.dto';
 import { REFRESH_TOKEN_COOKIE_NAME } from '../auth.constants';
-import { AuthGuard } from '../guards/auth.guard';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Post('login')
   async login(
@@ -22,22 +25,24 @@ export class AuthController {
     const { accessToken, refreshToken } =
       await this.authService.createAuthTokens(user._id.toString());
 
+    const isProductionEnv = this.configService.get('APP_ENV') === 'production';
     res.cookie(REFRESH_TOKEN_COOKIE_NAME, refreshToken, {
       httpOnly: true,
-      secure: true,
-      sameSite: 'strict',
+      secure: isProductionEnv,
+      sameSite: isProductionEnv ? 'strict' : 'lax',
     });
 
     return { accessToken, user };
   }
 
-  @UseGuards(AuthGuard)
   @Post('logout')
   async logout(@Res({ passthrough: true }) res: Response) {
+    const isProductionEnv = this.configService.get('APP_ENV') === 'production';
+
     res.clearCookie(REFRESH_TOKEN_COOKIE_NAME, {
       httpOnly: true,
-      secure: true,
-      sameSite: 'strict',
+      secure: isProductionEnv,
+      sameSite: isProductionEnv ? 'strict' : 'lax',
     });
 
     return { message: 'Logged out successfully' };
--- server/src/main.ts
+++ server/src/main.ts
@@ -8,6 +8,20 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get(ConfigService);
 
+  if (config.get('APP_ENV') === 'production') {
+    app.enableCors({
+      allowedHeaders: ['content-type', 'authorization'],
+      origin: 'domain_name',
+      credentials: true,
+    });
+  } else {
+    app.enableCors({
+      allowedHeaders: ['content-type', 'authorization'],
+      origin: 'http://localhost:5173',
+      credentials: true,
+    });
+  }
+
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
 
