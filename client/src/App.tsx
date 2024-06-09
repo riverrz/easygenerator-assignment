76f4c19b2761f3ef60baad61f291cc18513f3426
riverrz <shivam123425@gmail.com>
2024-06-09 19:40:29 +0400

    Add error boundry

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,32 @@
 import { Provider as StateProvider } from "jotai";
 import { Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ErrorBoundary } from "react-error-boundary";
 
 import { Toaster } from "@/components/ui/toaster";
 import { HomePage } from "@/modules/home/home.page";
 import { LoginPage } from "@/modules/auth/login/login.page";
 import { SignupPage } from "@/modules/auth/signup/signup.page";
 import { store } from "@/store";
+import { GlobalErrorBoundry } from "./components/global-error-boundry";
 
 // Create a client
 const queryClient = new QueryClient();
 
 function App() {
   return (
-    <StateProvider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-        </Routes>
-      </QueryClientProvider>
-      <Toaster />
-    </StateProvider>
+    <ErrorBoundary fallback={<GlobalErrorBoundry />}>
+      <StateProvider store={store}>
+        <QueryClientProvider client={queryClient}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+          </Routes>
+        </QueryClientProvider>
+        <Toaster />
+      </StateProvider>
+    </ErrorBoundary>
   );
 }
 
---
+++ client/src/components/global-error-boundry.tsx
@@ -0,0 +1,7 @@
+export const GlobalErrorBoundry = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-xl text-red-600">Looks like something went wrong.</h1>
+    </div>
+  );
+};
