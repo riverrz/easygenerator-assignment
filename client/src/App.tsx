cc695c7d539e50389e832b16111d40e322ba74b3
riverrz <shivam123425@gmail.com>
2024-06-09 20:00:29 +0400

    Move out queryClient into separate file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { Provider as StateProvider } from "jotai";
 import { Routes, Route } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { ErrorBoundary } from "react-error-boundary";
 
 import { Toaster } from "@/components/ui/toaster";
@@ -8,10 +8,8 @@ import { HomePage } from "@/modules/home/home.page";
 import { LoginPage } from "@/modules/auth/login/login.page";
 import { SignupPage } from "@/modules/auth/signup/signup.page";
 import { store } from "@/store";
-import { GlobalErrorBoundry } from "./components/global-error-boundry";
-
-// Create a client
-const queryClient = new QueryClient();
+import { GlobalErrorBoundry } from "@/components/global-error-boundry";
+import { queryClient } from "@/lib/query";
 
 function App() {
   return (
